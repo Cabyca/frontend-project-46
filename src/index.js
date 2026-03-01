6d6d10e3911f9778d0ef1feb79ba3d415293ab66
Alex <cabyca@yandex.ru>
2026-03-01 22:33:12 +0300

    step 4 asscinema

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import getData from "./getData.js"
 import parser from "./parser.js"
+import buildAstTree from "./buildAstTree"
 
 const genDiff = (pathToFile1, pathToFile2, formatName = 'stylish') => {
 
     const [dataRaw1, extensionFile1] = getData(pathToFile1)
     const [dataRaw2, extensionFile2] = getData(pathToFile2)
 
-    // console.log(typeof dataRaw1, extensionFile1)
-    // console.log(typeof dataRaw2, extensionFile2)
+    //console.log(typeof dataRaw1, extensionFile1)
+    //console.log(typeof dataRaw2, extensionFile2)
 
     const data1 = parser(dataRaw1, extensionFile1)
     const data2 = parser(dataRaw2, extensionFile2)
 
-    // console.log(data1)
-    // console.log(data2)
+    console.log(data1) // object
+    console.log(data2) // object
 
     // Уже есть распарсенные данные
+
+    //Результатом работы функции genDiff() является строка
+
+    const astTree = buildAstTree(data1, data2)
+
+    console.log(typeof astTree)
     
 
 }
